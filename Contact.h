27801049c7ce4457a222cc8a22655f6558003b1b
Jimmy <greatlodeman@gmail.com>
2019-02-19 15:01:14 -0700

    Premade code

--- Contact.h
+++ Contact.h
@@ -5,4 +5,10 @@
 #ifndef CONTACTSPROJECT_CONTACT_H
 #define CONTACTSPROJECT_CONTACT_H
 
+typedef struct ContactNode_struct {
+    char contactName[50];
+    char contactPhoneNum[50];
+    struct ContactNode_struct* nextNodePtr;
+} ContactNode;
+
 #endif //CONTACTSPROJECT_CONTACT_H
--- LLContacts.h
+++ LLContacts.h
@@ -5,4 +5,14 @@
 #ifndef CONTACTSPROJECT_LLCONTACTS_H
 #define CONTACTSPROJECT_LLCONTACTS_H
 
+typedef struct linked_list_contacts_struct
+{
+    ContactNode* head;
+    int size;
+} llc;
+
+void InitContactList(llc* l);
+void InsertContact(llc* l, char* name, char* phone);
+void DisplayContactList(llc* l);
+
 #endif //CONTACTSPROJECT_LLCONTACTS_H
